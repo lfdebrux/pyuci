ec441af85b356cc235549e18e5263a9e12997391
Laurence de Bruxelles <lfdebrux@gmail.com>
2016-06-04 17:51:09 +0100

    get the value of a string value

--- cuci.pxd
+++ cuci.pxd
@@ -3,15 +3,21 @@ from libcpp cimport bool
 cdef extern from "uci.h":
 	cdef struct uci_context:
 		pass
+
 	cdef struct uci_package:
 		pass
-	cdef struct uci_ptr:
+
+	cdef struct uci_section:
 		pass
 
 	uci_context* uci_alloc_context()
 	void uci_free_context(uci_context* ctx)
 
+	int uci_set_confdir(uci_context* ctx, const char* dir)
+
 	int uci_load(uci_context* ctx, const char* config_name, uci_package** package)
 	int uci_unload(uci_context* ctx, uci_package* package)
 
-	int uci_lookup_ptr(uci_context* ctx, uci_ptr* ptr, char* query, bool extended)
+	uci_package* uci_lookup_package(uci_context* ctx, const char* package_name)
+	uci_section* uci_lookup_section(uci_context* ctx, uci_package* package, const char* section_name)
+	const char* uci_lookup_option_string(uci_context* ctx, uci_section* section, const char* option_name)
--- uci.pyx
+++ uci.pyx
@@ -1,7 +1,26 @@
+# cython: c_string_type=str, c_string_encoding=ascii
+# ^^ implicit string decoding
+
 cimport cuci
 
+from ConfigParser import Error, \
+                         NoSectionError, \
+                         NoOptionError
+
+
+class NoConfigError(Error):
+    """A matching config file was not found"""
+
+    def __init__(self, config):
+        Error.__init__(self, 'No config: %r' % (config,))
+        self.config = config
+        self.args = (config,)
+
+
 cdef class Config:
+
     cdef cuci.uci_context* _ctx
+
     def __cinit__(self):
         self._ctx = cuci.uci_alloc_context()
         if self._ctx is NULL:
@@ -10,3 +29,32 @@ cdef class Config:
     def __dealloc__(self):
         if self._ctx is not NULL:
             cuci.uci_free_context(self._ctx)
+
+    def __init__(self, confdir=None):
+        if confdir is not None:
+            cuci.uci_set_confdir(self._ctx, confdir)
+
+    def get(self, config, section, option):
+        '''look up option'''
+
+        # have we loaded this package before?
+        cdef cuci.uci_package* p = cuci.uci_lookup_package(self._ctx, config)
+        if p is NULL:
+            # try loading the package
+            cuci.uci_load(self._ctx, config, &p)
+        if p is NULL:
+            raise NoConfigError(config)
+
+        # look up the section
+        cdef cuci.uci_section* s = cuci.uci_lookup_section(self._ctx, p, section)
+        if s is NULL:
+            raise NoSectionError(section)
+
+        # look up the option
+        cdef char* c_option_value = cuci.uci_lookup_option_string(self._ctx, s, option)
+        if c_option_value is NULL:
+            raise NoOptionError(option, section)
+
+        cdef object option_value = c_option_value
+
+        return option_value
