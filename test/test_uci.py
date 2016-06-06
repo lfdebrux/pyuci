75ca0107262f3760a3ebdf133f88708368d96069
Laurence de Bruxelles <lfdebrux@gmail.com>
2016-06-06 01:23:31 +0100

    test that appropriate exceptions are raised

--- test/test_uci.py
+++ test/test_uci.py
@@ -10,6 +10,15 @@ class TestUCI(unittest.TestCase):
     def setUp(self):
         self.c = uci.UCI(CONFDIR)
 
+    def test_no_config(self):
+        self.assertRaises(uci.NoConfigError, self.c.get, 'foo', 'bar', 'baz')
+
+    def test_no_section(self):
+        self.assertRaises(uci.NoSectionError, self.c.get, 'example', 'bar', 'baz')
+
+    def test_no_option(self):
+        self.assertRaises(uci.NoOptionError, self.c.get, 'example', 'test', 'baz')
+
     def test_get_string(self):
         s = self.c.get('example', 'test', 'string')
         self.assertEqual(s, 'some value')
