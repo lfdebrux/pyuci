4156c49fc05a2b64ca231f0800cca7588155c872
Laurence de Bruxelles <lfdebrux@gmail.com>
2016-06-06 09:02:15 +0100

    testing with setup.py test
    use setuptools as recommended by python packaging authority
    
    added some metadata
    
    created test suite and added it to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 setup(
+    name="pyuci",
+    description="OpenWRT Universal Configuration Interface for Python",
+    author="Laurence de Bruxelles",
+    author_email="lfdebrux@gmail.com",
+    test_suite="test",
     ext_modules=cythonize([
         Extension("uci", ["uci.pyx"],
                   libraries=["uci"])])
