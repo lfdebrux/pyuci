73583b595b800d2d7c02bf77d5d19f25983851f8
Laurence de Bruxelles <lfdebrux@gmail.com>
2016-06-06 11:38:13 +0100

    add to github

---
+++ README.rst
@@ -0,0 +1,9 @@
+=====
+pyuci
+=====
+
+A Python library for OpenWRT's *U*niversal *C*onfiguration *I*nterface.
+
+It wraps the `uci.h` C interface with a `UCI` object that looks a little like `ConfigParser`.
+
+WIP. Currently it is readonly.
--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from Cython.Build import cythonize
 
 setup(
     name="pyuci",
+    version="0.1.0.dev1",
     description="OpenWRT Universal Configuration Interface for Python",
     author="Laurence de Bruxelles",
     author_email="lfdebrux@gmail.com",
+    url="https://github.com/lfdebrux/pyuci",
     test_suite="test",
     ext_modules=cythonize([
         Extension("uci", ["uci.pyx"],
